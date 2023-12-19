d0705f6f0c36295c5e431fc72abeb5199ac33c00
Andres-AA <andres.am008@gmail.com>
2023-12-18 21:38:05 -0600

    Facturas

---
+++ PIV_PF_ProyectoFinal/Controllers/ClientesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PIV_PF_ProyectoFinal;
+
+namespace PIV_PF_ProyectoFinal.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private testEntities db = new testEntities();
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View(db.CLIENTES.ToList());
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CLIENTES cLIENTES = db.CLIENTES.Find(id);
+            if (cLIENTES == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cLIENTES);
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCliente,NombreCliente,CorreoElectronico")] CLIENTES cLIENTES)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CLIENTES.Add(cLIENTES);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cLIENTES);
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CLIENTES cLIENTES = db.CLIENTES.Find(id);
+            if (cLIENTES == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cLIENTES);
+        }
+
+        // POST: Clientes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCliente,NombreCliente,CorreoElectronico")] CLIENTES cLIENTES)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cLIENTES).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cLIENTES);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CLIENTES cLIENTES = db.CLIENTES.Find(id);
+            if (cLIENTES == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cLIENTES);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CLIENTES cLIENTES = db.CLIENTES.Find(id);
+            db.CLIENTES.Remove(cLIENTES);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
